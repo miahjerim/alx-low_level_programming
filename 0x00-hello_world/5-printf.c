3d9507888cb96946dddc2e3db66e56496312fed0
miahjerim <miahjerim@gmail.com>
2023-03-16 18:20:48 -0700

    prints exactly followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -this prints the content followed by a new line
+ *
+ * Return: always is 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
