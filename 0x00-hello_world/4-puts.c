78ef90efa9e76783618c70352002db6761a10b16
miahjerim <miahjerim@gmail.com>
2023-03-16 17:25:16 -0700

    compiling 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - My first main function
+ *
+ * Return: is always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
