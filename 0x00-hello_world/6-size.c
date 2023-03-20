22421303eb02dc1d078251af43b843d0ba73272f
miahjerim <miahjerim@gmail.com>
2023-03-20 03:32:22 -0700

    correction on task 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * main - The code first prints the size of a char,int,long int,long long int,float, on the computer it is compiled and run on.
- *
- * Return: always 0 on success
- */
+*main-prints the size of a char,int,long int,long long int,float,
+*on the computer its run on.
+*
+*Return: always 0 on success
+*/
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
