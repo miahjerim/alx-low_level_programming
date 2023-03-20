cd5fd4454353c59246200dbd06853aa24419e672
miahjerim <miahjerim@gmail.com>
2023-03-20 04:25:00 -0700

    correction on task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
-*main-prints the size of var on the computer its run on.
+*main-prints the size of vars on the computer its run on.
 *Return: always 0 on success
 */
 
 int main(void)
 {
-	int a;
-	char b;
+	char a;
+	int b;
 	long int c;
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
