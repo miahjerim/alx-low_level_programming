64e2c06ed19115d08c92f7a18407be09746d29c8
miahjerim <miahjerim@gmail.com>
2023-04-13 17:21:49 -0700

    101 mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 #define ERR_MSG "Error"
 
 /**
@@ -21,8 +22,12 @@ int is_digit(char *s)
 	}
 	return (1);
 }
+
 /**
- * _strlen - returns the length of the string
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
@@ -34,6 +39,7 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
 /**
  * errors - handles errors for main
  */
@@ -42,9 +48,10 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
+
 /**
  * main - multiplies two positive numbers
- * @argc: nmber of arguments
+ * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: always 0 (Success)
@@ -57,32 +64,31 @@ int main(int argc, char *argv[])
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof (int) * len);
-	if(!result)
+	result = malloc(sizeof(int) * len);
+	if (!result)
 		return (1);
-	for(i = 0; i <= len1 + len2; i++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for(len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
-			carry /=10;
+			carry /= 10;
 		}
-		if(carry > 0)
+		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
-		if(result[i])
+		if (result[i])
 			a = 1;
 		if (a)
 			putchar(result[i] + '0');
@@ -93,4 +99,3 @@ int main(int argc, char *argv[])
 	free(result);
 	return (0);
 }
-
