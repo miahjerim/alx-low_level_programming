c3b9a7caefeccbcd44fa60bd4b6bf92f8454acfb
miahjerim <miahjerim@gmail.com>
2023-04-28 02:15:00 +0300

    fix: stdlib headerfile removed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * print_list - Prints all elements in the list
  *
- * @h: The pointer to the list_t to be printed
+ * @h: The pointer to the list_t list
  *
  * Return: The number of elements in the node
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -21,8 +20,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		n++;
+		nodes++;
 	}
 
-	return (n);
+	return (nodes);
 }
