96ba24d818a154900180cb31b2ca38eb60b13c82
miahjerim <miahjerim@gmail.com>
2023-06-20 01:35:00 +0300

    0x18 all files

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -  a function that returns the length of a string
+ * @s: An input string
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(s);
+	return (i);
+}
